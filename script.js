e5b167aba0e4d6edb1c85bbf52a90081e05ac00f
rubydevi <ariesabyss@gmail.com>
2023-05-22 19:43:07 +0530

    Add the function of nav item click

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ openNav.addEventListener("click", () => {
 closeNav.addEventListener("click", () => {
   modalNav.style.display = "none";
 });
+
+const menuLists = document.querySelectorAll('#menuList li');
+
+menuLists.forEach((lists) => {
+  lists.addEventListener('click', (event) => {
+    const link = lists.querySelector('a');
+    const sectionId = link.getAttribute('href');
+
+    // Navigate to the target section
+    document.querySelector(sectionId).scrollIntoView();
+
+    // Hide the modal <ul>
+    const modalSection = document.getElementById('modalSetting');
+    modalSection.style.display = 'none';
+
+    // Prevent the default link behavior
+    event.preventDefault();
+  });
+});
