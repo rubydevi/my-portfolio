8d085198ab2a7999882144b53c027cfc74f04374
H-Ramalan <talktoramalan@gmail.com>
2023-05-22 06:17:29 -0700

    Add open nav function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const openNav = document.getElementById("btnSetting");
 const modalNav = document.getElementById("modalSetting");
 const closeNav = document.getElementById("btnCloseNav");
+
+openNav.addEventListener("click", () => {
+  modalNav.style.display = "flex";
+});
