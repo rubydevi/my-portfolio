6f40681a9ebe04ecb0ee780705dec460613b8b00
rubydevi <ariesabyss@gmail.com>
2023-05-23 13:58:38 +0530

    Dynamically add a function to populate all the projects in a grid

--- script.js
+++ script.js
@@ -30,55 +30,73 @@ menuLists.forEach((lists) => {
 });
 
 // Modal Popup for Projects
-let projects = [
+const projects = [
   {
-    name: "Profesional Art Printing Data",
+    name: 'Profesional Art Printing Data',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    image: "images/work1.svg",
-    technologies: ["HTML", "Bootstrap", "Ruby"],
-    liveLink: "https://example.com/project1",
-    sourceLink: "https://github.com/project1"
+    image: 'images/work1.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://example.com/project1',
+    sourceLink: 'https://github.com/project1',
   },
   {
-    name: "Data Dashboard Healthcare",
+    name: 'Data Dashboard Healthcare',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    image: "images/work2.svg",
-    technologies: ["HTML", "Bootstrap", "Ruby"],
-    liveLink: "https://example.com/project1",
-    sourceLink: "https://github.com/project1"
+    image: 'images/work2.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://example.com/project1',
+    sourceLink: 'https://github.com/project1',
   },
   {
-    name: "Website Portfolio 1",
+    name: 'Website Portfolio 1',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    image: "images/work2.svg",
-    technologies: ["HTML", "Bootstrap", "Ruby"],
-    liveLink: "https://example.com/project1",
-    sourceLink: "https://github.com/project1"
+    image: 'images/work2.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://example.com/project1',
+    sourceLink: 'https://github.com/project1',
   },
   {
-    name: "Profesional Art Printing Data",
+    name: 'Profesional Art Printing Data',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    image: "images/work2.svg",
-    technologies: ["HTML", "Bootstrap", "Ruby"],
-    liveLink: "https://example.com/project1",
-    sourceLink: "https://github.com/project1"
+    image: 'images/work2.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://example.com/project1',
+    sourceLink: 'https://github.com/project1',
   },
   {
-    name: "Data Dashboard Healthcare",
+    name: 'Data Dashboard Healthcare',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    image: "images/work2.svg",
-    technologies: ["HTML", "Bootstrap", "Ruby"],
-    liveLink: "https://example.com/project1",
-    sourceLink: "https://github.com/project1"
+    image: 'images/work2.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://example.com/project1',
+    sourceLink: 'https://github.com/project1',
   },
   {
-    name: "Website Portfolio 2",
+    name: 'Website Portfolio 2',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
-    image: "images/work2.svg",
-    technologies: ["HTML", "Bootstrap", "Ruby"],
-    liveLink: "https://example.com/project1",
-    sourceLink: "https://github.com/project1"
-  }
+    image: 'images/work2.svg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://example.com/project1',
+    sourceLink: 'https://github.com/project1',
+  },
 ];
 
-let projectsContainer = document.getElementById("projectsContainer"); // .card grid in my case
+const projectsContainer = document.getElementById('projectsContainer'); // .card grid in my case
+projects.forEach((project) => {
+  const projectElement = document.createElement('div'); // div for each project = projectElement
+  projectElement.classList.add('card', 'bg-image3', 'child-card');
+
+  // Create HTML structure for each project
+  projectElement.innerHTML = `
+    <div class="card-body">
+      <h2 class="card-title text-white child-title">${project.name} </h2>
+      <p class="card-description text-white child-description">${project.description}</p>
+      <ul class="card-tags child-tags">
+        ${project.technologies.map((techTag) => `<li class="card-tag child-tag">${techTag}</li>`).join('')}          
+      </ul>
+      <button type="button" class="card-button child-button">See project</button>
+    </div>
+  `;
+
+  projectsContainer.appendChild(projectElement); // append all the cards inside .card-grid div
+});
