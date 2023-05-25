ac8f0d2eef3c5495c930236b2b83399b019817f0
rubydevi <ariesabyss@gmail.com>
2023-05-25 15:19:12 +0530

    Create new js file for store data

---
+++ storedata.js
@@ -0,0 +1,5 @@
+// You should implement the following interactions:
+// When the user changes the content of any input field, the data is saved to the local storage.
+// When the user loads the page, if there is any data in the local storage the input fields are pre-filled with this data.
+// You should use the following data model:
+// You must create a single JavaScript object with all the data from the entire form and save it in local storage. Do not create one local storage entry per input field.
