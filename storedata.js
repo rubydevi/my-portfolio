e659c80f5f5a5b3f29be4da35c6e033a7005a8dc
rubydevi <ariesabyss@gmail.com>
2023-05-25 17:40:15 +0530

    create a single JavaScript object with all the data from the entire form and save it in local storage

--- storedata.js
+++ storedata.js
@@ -7,3 +7,18 @@ const prefilledData = () => {
     document.getElementById('txt_message').value = formData.message || '';
   }
 };
+
+window.onload = prefilledData();
+
+// Create a single JavaScript object with all the data from the entire form and save it in local storage
+const formInputs = document.querySelectorAll('#form_contact input, #form_contact textarea');
+formInputs.forEach((input) => {
+  input.addEventListener('input', () => {
+    const formData = {
+      name: document.getElementById('txtFullname').value,
+      email: document.getElementById('txtEmail').value,
+      message: document.getElementById('txt_message').value,
+    };
+    localStorage.setItem('formData', JSON.stringify(formData));
+  });
+});
