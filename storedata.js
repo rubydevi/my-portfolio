7b460faf29d508a14d4ff2b8df8d1f465869a930
rubydevi <ariesabyss@gmail.com>
2023-05-25 15:44:18 +0530

    Craete Form Data object

--- storedata.js
+++ storedata.js
@@ -1,5 +1,9 @@
-// You should implement the following interactions:
-// When the user changes the content of any input field, the data is saved to the local storage.
-// When the user loads the page, if there is any data in the local storage the input fields are pre-filled with this data.
-// You should use the following data model:
-// You must create a single JavaScript object with all the data from the entire form and save it in local storage. Do not create one local storage entry per input field.
+const formData = {};
+
+const fullName = document.getElementById('txtFullname');
+const emailId = document.getElementById('txtEmail');
+const textMessage = document.getElementById('txt_message');
+
+formData.name = fullName;
+formData.email = emailId;
+formData.message = textMessage;
